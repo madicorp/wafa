f51fa04a97b4e99d6fa070c9be7495750fd1be8c
Sega Diop <contact@madi-corp.net>
2017-03-13 18:15:07 +0000

    [module_site_vitrine] Home page fait - About page 100%

--- wafa/static/js/wafa.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    var swiper = new Swiper('.swiper-containers', {
-        slidesPerView: 'auto',
-        centeredSlides: true,
-        autoplay: 5000,
-        autoplayDisableOnInteraction: true
-    });
-});
