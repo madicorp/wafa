bf89aa4b956fddee32886adfe5ae787f3ca853f3
Sega Diop <contact@madi-corp.net>
2017-04-20 01:34:52 +0000

    Complete WAFA V1

--- wafa/settings/production.py
+++ wafa/settings/production.py
@@ -20,14 +20,6 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-COMPRESS_OFFLINE = True
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CSSMinFilter',
-]
-COMPRESS_CSS_HASHING_METHOD = 'content'
 
 ALLOWED_HOSTS = env['ALLOWED_HOSTS'].split(',')
 CONTACT_EMAIL = env['CONTACT_EMAIL']
