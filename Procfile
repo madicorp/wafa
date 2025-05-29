f52941fcb1162cfb61cbf6063e3ffe5c2485ba4d
segtio <segito10@gmail.com>
2025-05-29 21:10:42 +0200

    changes from meet May 9, 2025 (fix 500 on debug false)

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: ./manage.py migrate --no-input
-web: gunicorn wafa.wsgi --timeout 60 --keep-alive 5 --log-level debug
+release: python manage.py migrate --no-input && python manage.py collectstatic --noinput
+web: gunicorn wafa.wsgi --timeout 60 --keep-alive 5 --log-level info
--- wafa/settings/production.py
+++ wafa/settings/production.py
@@ -1,63 +1,72 @@
 from __future__ import absolute_import, unicode_literals
 
+import os
 import dj_database_url
-
-
 from .base import *
 
 env = os.environ.copy()
-DEBUG = env['DEBUG'] == "True"
 
-SECRET_KEY = env['SECRET_KEY']
-db_from_env = dj_database_url.config()
+# Debug
+DEBUG = env.get("DEBUG", "False").lower() in ("true", "1", "yes", "True")
+
+# Secret key
+SECRET_KEY = env.get("SECRET_KEY", "")
+
+# Allowed Hosts
+ALLOWED_HOSTS = env.get("ALLOWED_HOSTS", "").split(",")
+
+# Email
+CONTACT_EMAIL = env.get("CONTACT_EMAIL", "")
+EMAIL_HOST = env.get("EMAIL_HOST", "")
+EMAIL_PORT = int(env.get("EMAIL_PORT", 587))  # cast to int
+EMAIL_HOST_USER = env.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = env.get("EMAIL_HOST_PASSWORD", "")
+
+# Database
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % env['AWS_STORAGE_BUCKET_NAME']
+# AWS S3 Storage
+AWS_STORAGE_BUCKET_NAME = env.get("AWS_STORAGE_BUCKET_NAME", "")
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
-MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STORAGES = {
     'default': {
         'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
         'OPTIONS': {
-            'access_key': env['AWS_ACCESS_KEY_ID'],
-            'secret_key': env['AWS_SECRET_ACCESS_KEY'],
-            'bucket_name': env['AWS_STORAGE_BUCKET_NAME'],
-            'default_acl': env['AWS_DEFAULT_ACL'],
+            'access_key': env.get('AWS_ACCESS_KEY_ID', ''),
+            'secret_key': env.get('AWS_SECRET_ACCESS_KEY', ''),
+            'bucket_name': AWS_STORAGE_BUCKET_NAME,
+            'default_acl': env.get('AWS_DEFAULT_ACL', 'public-read'),
             'custom_domain': AWS_S3_CUSTOM_DOMAIN,
         },
     },
     'staticfiles': {
-        'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
         'OPTIONS': {
             'location': STATIC_ROOT,
         },
     },
 }
 
-
-ALLOWED_HOSTS = env['ALLOWED_HOSTS'].split(',')
-CONTACT_EMAIL = env['CONTACT_EMAIL']
-EMAIL_HOST = env['EMAIL_HOST']
-EMAIL_PORT = env['EMAIL_PORT']
-EMAIL_HOST_USER = env['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = env['EMAIL_HOST_PASSWORD']
-
+# Logging (only in production)
 if not DEBUG:
     LOGGING = {
         'version': 1,
         'disable_existing_loggers': False,
         'filters': {
             'require_debug_false': {
-                '()': 'django.utils.log.RequireDebugFalse'
+                '()': 'django.utils.log.RequireDebugFalse',
             }
         },
         'handlers': {
             'mail_admins': {
                 'level': 'ERROR',
                 'filters': ['require_debug_false'],
-                'class': 'django.utils.log.AdminEmailHandler'
+                'class': 'django.utils.log.AdminEmailHandler',
             }
         },
         'loggers': {
@@ -65,13 +74,15 @@ if not DEBUG:
                 'handlers': ['mail_admins'],
                 'level': 'ERROR',
                 'propagate': True,
-            },
+            }
         }
     }
 
+# Google Maps API Key for location_field
 from location_field import settings as location_field_settings
-location_field_settings.LOCATION_FIELD["provider.google.api_key"] = env['GEOPOSITION_GOOGLE_MAPS_API_KEY']
+location_field_settings.LOCATION_FIELD["provider.google.api_key"] = env.get("GEOPOSITION_GOOGLE_MAPS_API_KEY", "")
 
+# Optional local overrides
 try:
     from .local import *
 except ImportError:
